85595766328edcdcb6bc07f900ec29662ba88f06
cy360124 <cy360124@163.com>
2020-01-27 19:36:06 +0800

    add inc/types.h

---
+++ inc/types.h
@@ -0,0 +1,16 @@
+#ifndef INC_TYPES_H
+#define INC_TYPES_H
+
+#define NULL ((void*) 0)
+
+// Explicitly-sized versions of integer types
+typedef signed char int8_t;
+typedef unsigned char int8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
+
+#endif // !INC_TYPES_H
